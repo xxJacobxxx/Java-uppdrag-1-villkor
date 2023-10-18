a33ccd83efc9a559ebdab3a562d3e836c26bfad0
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-10-18 10:09:43 +0200

    GJorde första delen: a)

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    int litetTal=10;
+    int stortTal=1000;
+    if(litetTal<1000)
+{
+        System.out.println(litetTal +"är mindre än"+stortTal);
+}
+    else
+{
+    System.out.println(litetTal +"är mindre än"+stortTal);
+}
+    System.out.println("litetTal="+litetTal+"stortTal="+stortTal);
+    }
+}
+
+
