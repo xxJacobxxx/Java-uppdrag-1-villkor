c5c17108c357329a8ff3b1d1c78159153739e8f8
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-10-18 10:33:26 +0200

    GJorde d är på e nu

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-    int litetTal=10;
+/*     int litetTal=10;
     int stortTal=1000;
     if(litetTal<1000)
 {
@@ -11,7 +12,40 @@ public class App {
     System.out.println(litetTal +"är mindre än"+stortTal);
 }
     System.out.println("litetTal="+litetTal+"stortTal="+stortTal);
-    }
+
+
+int tal1=10;
+int tal2=20;
+if(tal2>=tal1){
+    System.out.println(tal2+" är större än "+tal1);
 }
+else{
+    System.out.println(tal2+"är inte större än "+tal1);
+}
+    Scanner tb = new Scanner(System.in);
+    int clock=tb.nextInt();
+    if(clock>=6){
+        System.out.println("God Natt");
+    }
+    else if(clock>=10){
+        System.out.println("God förmiddag");
+    }
+    else if(clock>=15){
+        System.out.println("God eftermiddag");
+    }
+    else if(clock>=19){
+        System.out.println("God kväll");
+    }
+    else if(clock>=23){
+    System.out.println("God natt");
+    }
+    else if(clock>=24){
+        System.out.println("?????");
+    }
+    */ 
+    
+}
+}
+
 
 
